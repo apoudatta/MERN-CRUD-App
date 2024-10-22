6042b824a459c82eedc5e75e1a89beb2aee7e778
apoudatta <apoudatto6@gmail.com>
2024-10-22 18:20:49 +0600

    itemRoute add

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -4,3 +4,34 @@ const Item = require('../models/Item');
 
 // @route GET /api/items
 // @desc Get all items
+router.get('/', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+// @route POST /api/items
+// @desc Create a new item
+router.post('/', async (req, res) => { 
+    const newItem = new Item({
+        name: req.body.name
+    });
+    const savedItem = await newItem.save();
+    res.json(savedItem);
+});
+
+// @route PUT /api/items/:id
+// @desc  Update an item
+router.put('/:id', async (req, res) => {
+    const updateItem = await Item.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+    res.json(updateItem);
+});
+
+// @route DELETE /api/items/:id
+// @desc  Update an item
+router.delete('/:id', async (req, res) => { 
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Item deleted' });
+});
+
+module.exports = router;
+
